0d03ef5339cc1a848a0f16f198dbacc1beeadc9e
Armando Jr <elfiservice@hotmail.com>
2018-04-27 06:54:41 -0300

    feat: BookshelfApp done and tested with shelves[]

--- src/BookshelfApp.js
+++ src/BookshelfApp.js
@@ -3,10 +3,26 @@ import logo from './logo.svg';
 import './BookshelfApp.css';
 
 class BookshelfApp extends Component {
+    constructor(props) {
+        super(props);
+        this.shelves = [
+            'currently reading',
+            'want to read',
+            'read'
+        ]
+        
+    }
   render() {
     return (
       <div className="App">
         My Bookshelf
+        <main>
+            {this.shelves.map( shelf => (
+               <section>
+                   <h2>{shelf}</h2>
+               </section> 
+            ))}
+        </main>
       </div>
     );
   }
