d639bfaa2e72e0627c6b6db6085fa554b47f1dea
Armando Jr <elfiservice@hotmail.com>
2018-04-28 07:45:49 -0300

    feat: provided selects to each book to change shelf

--- src/Book.js
+++ src/Book.js
@@ -6,6 +6,13 @@ class Book extends Component {
         return (
             <div className="book-content">
                 <img src={book.imageLinks.thumbnail} alt={book.title + " book"} />
+                <select>
+                    <option disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want To Read</option>
+                    <option value="read">Read</option>
+                    <option value="">None</option>
+                </select>
             </div>
         )
     }
