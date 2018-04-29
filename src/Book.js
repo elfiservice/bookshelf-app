4c7e054be4f3848690c87f26f30feccbc77791c6
Armando Jr <elfiservice@hotmail.com>
2018-04-29 20:15:21 -0300

    feat: style select elemt like a button in each book

--- src/Book.js
+++ src/Book.js
@@ -7,14 +7,17 @@ class Book extends Component {
         const onChangeShelf = this.props.changeShelf;
         return (
             <div className="book-content">
+                <div className="book-select-container">
                 <img src={book.imageLinks.thumbnail} alt={book.title + " book"} />
-                <select defaultValue={book.shelf} onChange={(e) => onChangeShelf(book, e.target.value)}>
-                    <option disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want To Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                </select>
+                    <svg className="book-select-icon"> </svg>
+                    <select className="book-select-content" defaultValue={book.shelf} onChange={(e) => onChangeShelf(book, e.target.value)}>
+                        <option disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want To Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
             </div>
         )
     }
