2e4224c9fa3914a7f6564ced1a759d9ff51bd1ad
Armando Jr <elfiservice@hotmail.com>
2018-04-30 06:49:58 -0300

    feat: add react-router-dom pck install in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import BookshelfApp from './BookshelfApp';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <BookshelfApp />, 
+    <BrowserRouter><BookshelfApp /></BrowserRouter>, 
     document.getElementById('root')
 );
 registerServiceWorker();
