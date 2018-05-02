60cc388094cf9524b313e36c60c5fdb0d5f6b467
Armando Jr <elfiservice@hotmail.com>
2018-05-02 08:25:20 -0300

    refactore: relocated API requests to main Parent of APP

--- src/BookshelfApp.js
+++ src/BookshelfApp.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI';
 import { Route } from 'react-router-dom';
 import './BookshelfApp.css';
 import Shelves from './Shelves';
 import SearchBooks from './SearchBooks';
 
 class BookshelfApp extends Component {
-    constructor(props) {
-        super(props);
-        this.shelves = [
-            { key: 'currentlyReading', name: 'Currently Reading'},
-            { key: 'wantToRead', name: 'Want To Read'},
-            { key: 'read', name: 'Read'}
-        ]
-
-        
+  constructor(props) {
+      super(props);
+      this.shelves = [
+          { key: 'currentlyReading', name: 'Currently Reading'},
+          { key: 'wantToRead', name: 'Want To Read'},
+          { key: 'read', name: 'Read'}
+      ]
+      this.state = {
+        listOfBooks: []
     }
+    this.changeShelf = this.changeShelf.bind(this);
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+        this.setState({ listOfBooks: books });
+    });     
+  }
+
+  changeShelf(book, shelf) {
+      alert('You change the ' + book.title + " book to " + shelf + " shelf!");
+      BooksAPI.update(book, shelf).then(data => {
+          let listOfBooks = this.state.listOfBooks;
+          for(let i = 0; i < listOfBooks.length; i++) {
+              if(listOfBooks[i].id === book.id) {
+                  listOfBooks[i].shelf = shelf;
+                  this.setState({ listOfBooks: listOfBooks })
+                }
+          }
+      });   
+  }
   render() {
     return (
       <div className="App">
@@ -24,7 +46,11 @@ class BookshelfApp extends Component {
           </header>
         </div>
         <Route exact path="/" render={() => (
-          <Shelves  shelvesNames={this.shelves}/>
+          <Shelves  
+            shelvesNames={this.shelves}
+            listOfBooks={this.state.listOfBooks}
+            onChangeShelf={this.changeShelf} 
+          />
         )} />
 
         <Route path="/search" render={() => (
--- src/Shelves.js
+++ src/Shelves.js
@@ -1,38 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import BookList from './BookList';
-import * as BooksAPI from './BooksAPI';
 
 class Shelves extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            listOfBooks: []
-        }
-        this.changeShelf = this.changeShelf.bind(this);
-    }
-
-    componentDidMount() {
-        BooksAPI.getAll().then(books => {
-            this.setState({ listOfBooks: books });
-        });     
-    }
-
-    changeShelf(book, shelf) {
-        alert('You change the ' + book.title + " book to " + shelf + " shelf!");
-        BooksAPI.update(book, shelf).then(data => {
-            let listOfBooks = this.state.listOfBooks;
-            for(let i = 0; i < listOfBooks.length; i++) {
-                if(listOfBooks[i].id === book.id) {
-                    listOfBooks[i].shelf = shelf;
-                    this.setState({ listOfBooks: listOfBooks })
-                  }
-            }
-        });   
-    }
 
     render() {
-        const shelvesNames = this.props.shelvesNames;
+        const { shelvesNames, listOfBooks, onChangeShelf } = this.props;
         
         return (
             <main className="container shelves">
@@ -40,7 +13,7 @@ class Shelves extends Component {
                     {shelvesNames.map( (shelf) => (
                     <section className="shelf-container" key={shelf.key}>
                         <h2>{shelf.name}</h2>
-                            <BookList shelfName={shelf} listOfBooks={this.state.listOfBooks} onChangeShelf={this.changeShelf} />
+                            <BookList shelfName={shelf} listOfBooks={listOfBooks} onChangeShelf={onChangeShelf} />
                     </section> 
                     ))}
                 </div>
